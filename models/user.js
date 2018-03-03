1c383b43cd8bbef9f98950bbcaafd7f174fe9096
Ejike <reyhenry38@gmail.com>
2018-03-03 11:44:08 -0500

    crested user js

--- models/user.js
+++ models/user.js
@@ -1,10 +1,27 @@
-const bcrypt = require("bcrypt-node.js");
+const bcrypt = require("bcrypt-nodejs");
 
 module.exports = (sequelize, DataTypes) => {
-    let User sequelize.define("User", {
-        email:{
+    let User = sequelize.define("User", {
+        email: {
             type: DataTypes.STRING,
-            all
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            },
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false
         }
+    });
+
+    User.prototype.validPassword = function (password) {
+        return bcrypt.compareSync(password, this.password);
+    };
+
+    User.hook("beforeCreate", function (user) {
+        user.password = bcrypt.hashSync(user.password, bcrypt.genSaltSync(10), null);
     })
+
 }
