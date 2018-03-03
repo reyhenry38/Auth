737fe4c58269ccfca11d812966b3b67b01fa263a
Ejike <reyhenry38@gmail.com>
2018-03-03 14:48:59 -0500

    modified server file

--- server.js
+++ server.js
@@ -1,35 +1,32 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-const session = require("express-sessions");
+const session = require("express-session");
+const passport = require("./config/passport");
 
 const PORT = process.env.PORT || 8080;
 
 let app = express();
+const db = require("./models");
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 app.use(express.static("public"));
 
-app.listen(PORT, () => {
-    console.log(`listening on port: ${PORT}`);
-    // console.log("listening on port:"+PORT);
+app.use(session({
+    secret: "keyboard cat", resave: true,
+    saveUninitialized: true
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+//require our routes 
+require("./routes/htmlRoutes")(app);
+require("./routes/apiRoutes")(app);
+
+db.sequelize.sync().then(() => {
+    app.listen(PORT, () => {
+        console.log(`listening on port: ${PORT}`)
+    })
 })
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-    var err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
