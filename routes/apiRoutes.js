e1a27ca675b599ca52c17792ff83e52b8864ef23
Ejike <reyhenry38@gmail.com>
2018-03-03 13:51:35 -0500

    User login

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,9 +1,23 @@
-const db = require ("../models");
-
-const passport = require("../config/passport");
+const db = require("../models");
+const passport = require("../config/passport.js");
 
 module.exports = function (app) {
-    app.post("api/login", passport.authenticate("local"), (req, res)=>{
+    app.post("api/login", passport.authenticate("local"), (req, res) => {
         res.json("/members");
     });
-}
+
+    app.post("api/signup", (req, res) => {
+        console.log(req.body);
+        db.User.create({
+            email: req.body.email,
+            password: req.body.password
+        }).then(() => {
+            res.redirect(307, "/api/login");
+        }).catch(function (err) {
+            console.log(err);
+            res.json(err);
+        });
+    });
+
+
+};
